0701f83940fc45e14a20a33aa9961f40a762d044
root <root@LAPTOP-GF3MT963>
2024-07-10 20:23:02 +0800

    mmu_fix

--- rtl/a_mmu_defines.svh
+++ rtl/a_mmu_defines.svh
@@ -1,5 +1,7 @@
 //a_csr.svh里面和tlb相关的内容还有wired的标记，最好删掉
 
+`define _TLB_ENTRY_NUM 64
+
 //mem_type
 `define _MEM_FETCH  `2'h0
 `define _MEM_LOAD   `2'h1
@@ -12,30 +14,16 @@
 `define _CRMD_DATF  6:5
 `define _CRMD_DATM  8:7
 
-`define _DMW_PLV0   0
-`define _DMW_PLV3   3
-`define _DMW_PSEG   27:25
-`define _DMW_VSEG   31:29
-
 //tlb
-typedef struct packed {
-    logic           e;
-    logic [9:0]  asid;
-    logic           g;
-    logic     huge_ps;
-    logic [18:0] vppn;
-} tlb_key_t;
-
-typedef struct packed {
-    logic           v;
-    logic           d;
-    logic [1:0]   mat;
-    logic [1:0]   plv;
-    logic [19:0]  ppn;
-} tlb_value_t;
-
 typedef struct packed {
     logic [31:0]  pa;
     logic [1:0]  mat;
     logic      valid;
+    exception_t exception;
 } trans_result_t;
+
+typedef struct packed {
+    logic [`_TLB_ENTRY_NUM - 1 : 0] tlb_rd_index;
+    logic [`_TLB_ENTRY_NUM - 1 : 0] tlb_wr_index;
+    tlb_entry_t tlb_wr_entry;
+} tlb_rdwr_req_t;
--- rtl/basic_mmu.sv
+++ rtl/basic_mmu.sv
@@ -1,10 +1,10 @@
 `include "a_defines.svh"
 
 //用寄存器存的tlb表项，打一拍出结果
-//目前完成地址翻译，错误码和tlb的读写支持待补充
+//目前错误码待补充
+//待测试
 module mmu #(
-    parameter TLB_ENTRY_NUM = 64,
-    parameter TLB_SWITCH_OFF = 0
+    parameter TLB_SWITCH_OFF = 0 //这个暂时没用
 ) (
     input  wire  clk,
     input  wire  rst_n,
@@ -14,24 +14,28 @@ module mmu #(
     input  csr_t                  csr,
     input  wire  [1:0]            mem_type,//类型，定义见a_mmu_defines
 
-    output trans_result_t trans_result_o
+    input tlb_rdwr_req_t  tlb_rdwr_req_t,  //tlb维护的请求，包括读/写的index，见a_mmu_defines
+
+    output trans_result_t trans_result_o,//包含pa，mat，valid，见a_mmu_defines
+    output tlb_entry_t    tlb_entry_o    //tlb维护（读）时找到的tlb_entry，暂时写成一拍后得到结果，但也可以马上得到
 )
 
+//查tlb
 wire cur_asid = csr.asid;
 logic tlb_found;
 logic tlb_entry_t tlb_value_read;
 
-logic tlb_key_t   [TLB_ENTRY_NUM - 1:0]      tlb_key_q;
-logic tlb_entry_t [1:0][TLB_ENTRY_NUM - 1:0] tlb_entry_q;
+logic tlb_key_t   [`_TLB_ENTRY_NUM - 1:0]      tlb_key_q;
+logic tlb_value_t [1:0][`_TLB_ENTRY_NUM - 1:0] tlb_value_q;
 
 always_comb begin
     tlb_found = 0;
-    for (integer i = 0; i < TLB_ENTRY_NUM; i+= 1) begin
+    for (integer i = 0; i < `_TLB_ENTRY_NUM; i+= 1) begin
         if (tlb_key_q[i].e 
-        && (tlb_entry_q[i].g || (tlb_entry_q[i].asid == cur_asid))
-        && vppn_match(va, tlb_key_q[i].huge_ps)) begin
+        && (tlb_key_q[i].g || (tlb_key_q[i].asid == cur_asid))
+        && vppn_match(va, tlb_key_q[i].huge_page)) begin
             tlb_found = 1;
-            if (tlb_key_q[i].huge_ps) begin
+            if (tlb_key_q[i].huge_page) begin
                 tlb_value_read = tlb_value_q[va[22]][i];   //4MB
             end else begin
                 tlb_value_read = tlb_value_q[va[12]][i]; //4KB
@@ -41,14 +45,31 @@ always_comb begin
 end
 
 function automatic logic vppn_match(logic [31:0] va, 
-                                    logic huge_ps)
-    if (huge_ps) begin
-        return va[31:23] == tlb_key.vppn[18:10]; ???
+                                    logic huge_page)
+    if (huge_page) begin
+        return va[31:23] == tlb_key.vppn[18:10]; //???is this right
     end else begin
         return va[31:13] == tlb_key.vppn;
     end
 endfunction
 
+//tlb读写请求
+always_ff @(posedge clk) begin
+    for (genvar i = 0; i < `_TLB_ENTRY_NUM; i += 1) begin
+        if (tlb_rdwr_req_t.tlb_wr_index[i]) begin
+            tlb_key_q      <= tlb_rdwr_req_t.tlb_wr_entry.key;
+            tlb_value_q[0] <= tlb_rdwr_req_t.tlb_wr_entry.value[0];
+            tlb_value_q[1] <= tlb_rdwr_req_t.tlb_wr_entry.value[1];
+        end
+        if (tlb_rdwr_req_t.tlb_rd_index[i]) begin
+            tlb_entry_o.key    <= tlb_key_q;
+            tlb_entry_o.value[0]    <= tlb_value_q[0];
+            tlb_entry_o.value[1]    <= tlb_value_q[1];
+        end
+    end
+end
+
+//查dmw
 wire [31:0] dmw0 = csr.dmw0;
 wire [31:0] dmw1 = csr.dmw1;
 
@@ -57,20 +78,20 @@ wire    plv3     = csr.crmd[`_CRMD_PLV] == 2'd3;
 wire dmw0_plv_ok = (plv0 && dmw0[`_DMW_PLV0]) || (plv3 && dmw0[`_DMW_PLV3]);
 wire dmw1_plv_ok = (plv0 && dmw1[`_DMW_PLV0]) || (plv3 && dmw1[`_DMW_PLV3]);
 
-wire    dmw0_hit = dmw0[`_DMW_VSEG] == va[31:29];
-wire    dmw1_hit = dmw1[`_DMW_VSEG] == va[31:29];
+wire    dmw0_hit = (dmw0[`_DMW_VSEG] == va[31:29]) && dmw0_plv_ok;
+wire    dmw1_hit = (dmw1[`_DMW_VSEG] == va[31:29]) && dmw1_plv_ok;
 
-logic        dmw_hit  = (dmw0_hit && dmw0_plv_ok) 
-                     || (dmw1_hit && dmw1_plv_ok);
-logic [31:0] dmw_read = dmw0_hit ? dmw0 :
-                        dmw1_hit ? dmw1 :
-                        '0;
+wire        dmw_hit  = dmw0_hit || dmw1_hit;
+wire [31:0] dmw_read = dmw0_hit ? dmw0 :
+                       dmw1_hit ? dmw1 :
+                       '0;
 
 logic trans_result_t trans_result;
 
-wire da = csr.crmd.`_CRMD_DA;
-wire pg = csr.crmd.`_CRMD_PG;
+wire da = csr.crmd[`_CRMD_DA];
+wire pg = csr.crmd[`_CRMD_PG];
 
+//choose from tlb/dmw/da
 always_comb begin
     if (da) begin
         trans_result.pa = va;
@@ -83,14 +104,14 @@ always_comb begin
             trans_result.mat = dmw_read[`_DMW_MAT];
             trans_result.valid = 1;
         end else begin
-            if (tlb_value_read.huge_ps) begin
+            if (tlb_value_read.huge_page) begin
                 trans_result.pa = {tlb_value_read.ppn, va[11:0]};
                 trans_result.mat = tlb_value_read.mat;
             end else begin
                 trans_result.pa = {tlb_value_read.ppn[19:10], va[21:0]};
                 trans_result.mat = tlb_value_read.mat;
             end
-            trans_result_valid = 1;
+            trans_result.valid = tlb_found;
             if (!tlb_value_read.v) begin
                 trans_result.valid = 0;
                 case (mem_type)
