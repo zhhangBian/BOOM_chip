47fd96a3be3eae2c49e662df42ec31a251aec050
汤睿璟 <22371027@buaa.edu.cn>
2024-07-20 19:33:50 +0800

    fix: npc

--- rtl/b_npc.sv
+++ rtl/b_npc.sv
@@ -6,41 +6,43 @@
 `include "define.svh"
 
 module f_npc (
-    input clk,
-    input rst_n,
-    
-    input  logic            ready_i,
-    output logic            valid_o,
+    input wire                  clk,
+    input wire                  rst_n,
+    input wire                  g_flush,
 
-    input  correct_info_t   correct_info_i, // 后端反馈的修正信息
-
-    output logic [31:0 ]    pc_o, // 指令PC，传递给ICACHE
-    output logic [ 1:0 ]    mask_o, // 掩码，表示当前的两条指令中那一条需要被取出来。比如2'b10表明偶数PC需要取，而奇数PC不需要
-    output predict_info_t   predict_info_o // 预测信息
+    input  correct_info_t [1:0] correct_infos_i, // 后端反馈的修正信息
+    handshake_if.sender         sender // predict_infos_t type
 );
 
 logic [31:0] pc;
+logic [31:0] npc;
+
+assign npc = {pc[31:3] + 1, 3'b000};
 
 always_ff @( clk ) begin : pc_logic
     if (!rst_n) begin
-        pc <= BPU_INIT_PC;
+        pc <= `BPU_INIT_PC;
     end
-    else if (correct_info_i.redir) begin
+    else if (g_flush) begin
         pc <= correct_info_i.redir_addr;
     end
     else if (ready_i) begin
-        if (pc[3]) begin
-            pc <= pc + 32'd4;
-        end
-        else begin
-            pc <= pc + 32'd8;
-        end
-    end
-    else begin
-        pc <= pc;
+        pc <= npc;
     end
 end
 
-assign pc_o = pc
+assign sender.data.pc = pc;
+assign sender.data.mask = pc[2] ? 2'b01 : 2'b11;
+
+predict_info_t predict_info;
+assign predict_info.target_pc   = npc;
+assign predict_info.is_branch   = '0;
+assign predict_info.br_type     = '0;
+assign predict_info.taken       = '0;
+assign predict_info.scnt        = '0;
+assign predict_info.need_update = '0;
+assign predict_info.history     = '0;
+
+assign sender.data.predict_info = predict_info;
     
 endmodule
