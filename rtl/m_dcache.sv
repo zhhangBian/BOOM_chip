0a1137dd7d5042e2fea3a3e23c7dba10b03ef064
Tanggling <1656801685@qq.com>
2024-07-23 22:16:59 +0800

    解决环路

--- rtl/m_dcache.sv
+++ rtl/m_dcache.sv
@@ -190,8 +190,7 @@ handshake_if #(.T(sb_entry_t)) sb_entry_sender();
 assign sb_entry_receiver.valid = !flush_i & !stall_q & |m1_iq_lsu_pkg.strb & valid_q;
 assign sb_entry_receiver.data  = w_sb_entry;
 assign sb_entry_sender.ready   = commit_cache_req.fetch_sb;/* commit提交sw指令请求 */
-assign r_sb_entry              = sb_entry_sender.data; 
-assign sb_entry_sender.data    = r_sb_entry;
+assign r_sb_entry              = sb_entry_sender.data;
 
 storebuffer #(
     .SB_SIZE(SB_SIZE)
