d5153f7cf991918e163094b7126f690dbf49bfd2
root <root@LAPTOP-GF3MT963>
2024-08-02 23:13:38 +0800

    mmu_提前打拍

--- basic_mmu_beifen.sv
+++ basic_mmu_beifen.sv
@@ -12,7 +12,7 @@ module mmu #(
     // 地址翻译
     input  logic [31:0]           va,
     input  csr_t                  csr,
-    input  wire  [1:0]            mmu_mem_type,  //类型，定义见a_mmu_defines // 
+    input  logic  [1:0]            mmu_mem_type,  //类型，定义见a_mmu_defines // 
     // 维护
     input  tlb_write_req_t  tlb_write_req_i,   //tlb维护的请求，包括写的独热码，见a_mmu_defines
 
@@ -20,6 +20,9 @@ module mmu #(
     output tlb_exception_t tlb_exception_o  //tlb相关的例外，例外代码的编号在a_csr里面，默认为零，只有在result里面valid为0的时候这个错误码才有意义
     //output tlb_entry_t    tlb_entry_o        //tlb维护（读）时找到的tlb_entry，暂时写成一拍后得到结果，但也可以马上得到
 );
+
+logic [31:0] va_q;
+logic [1:0]  mmu_mem_type_q;
 
 //查tlb
 wire  [9:0] cur_asid = csr.asid[`_ASID];
@@ -37,13 +40,13 @@ always_comb begin
     for (integer i = 0; i < TLB_ENTRY_NUM; i+= 1) begin
         if (tlb_key_q[i].e 
         && (tlb_key_q[i].g || (tlb_key_q[i].asid == cur_asid))
-        && vppn_match(va, tlb_key_q[i].huge_page, tlb_key_q[i].vppn)) begin
+        && vppn_match(va_q, tlb_key_q[i].huge_page, tlb_key_q[i].vppn)) begin
             tlb_found = 1;
             tlb_key_read = tlb_key_q[i];
             if (tlb_key_q[i].huge_page) begin
-                tlb_value_read = tlb_value_q[i][va[21]];   //4MB,2MB，TODO ？
+                tlb_value_read = tlb_value_q[i][va_q[21]];   //4MB,2MB，TODO ？
             end else begin
-                tlb_value_read = tlb_value_q[i][va[12]];   //4KB
+                tlb_value_read = tlb_value_q[i][va_q[12]];   //4KB
             end
         end
     end
@@ -78,8 +81,8 @@ wire    plv3     = csr.crmd[`_CRMD_PLV] == 2'd3;
 wire dmw0_plv_ok = (plv0 && dmw0[`_DMW_PLV0]) || (plv3 && dmw0[`_DMW_PLV3]);
 wire dmw1_plv_ok = (plv0 && dmw1[`_DMW_PLV0]) || (plv3 && dmw1[`_DMW_PLV3]);
 
-wire    dmw0_hit = (dmw0[`_DMW_VSEG] == va[31:29]) && dmw0_plv_ok;
-wire    dmw1_hit = (dmw1[`_DMW_VSEG] == va[31:29]) && dmw1_plv_ok;
+wire    dmw0_hit = (dmw0[`_DMW_VSEG] == va_q[31:29]) && dmw0_plv_ok;
+wire    dmw1_hit = (dmw1[`_DMW_VSEG] == va_q[31:29]) && dmw1_plv_ok;
 
 wire        dmw_hit  = dmw0_hit || dmw1_hit;
 wire [31:0] dmw_read = dmw0_hit ? dmw0 :
@@ -101,21 +104,21 @@ always_comb begin
     trans_result = '0;
 
     if (da) begin
-        trans_result.pa = va;
-        trans_result.mat = (mmu_mem_type == `_MEM_FETCH) ? 
+        trans_result.pa = va_q;
+        trans_result.mat = (mmu_mem_type_q == `_MEM_FETCH) ? 
             csr.crmd[`_CRMD_DATF] : csr.crmd[`_CRMD_DATM];
         trans_result.valid = 1;
     end else begin
         if (dmw_hit) begin
-            trans_result.pa = {dmw_read[`_DMW_PSEG],va[28:0]};
+            trans_result.pa = {dmw_read[`_DMW_PSEG],va_q[28:0]};
             trans_result.mat = dmw_read[`_DMW_MAT];
             trans_result.valid = 1;
         end else begin
             if (tlb_key_read.huge_page) begin //fixed
-                trans_result.pa = {tlb_value_read.ppn, va[11:0]};
+                trans_result.pa = {tlb_value_read.ppn, va_q[11:0]};
                 trans_result.mat = tlb_value_read.mat;
             end else begin
-                trans_result.pa = {tlb_value_read.ppn[19:10], va[21:0]};
+                trans_result.pa = {tlb_value_read.ppn[19:10], va_q[21:0]};
                 trans_result.mat = tlb_value_read.mat;
             end
             trans_result.valid = tlb_found;
@@ -123,7 +126,7 @@ always_comb begin
                 ecode = `_ECODE_TLBR;
             end else if (!tlb_value_read.v) begin
                 trans_result.valid = 0;
-                case (mmu_mem_type)
+                case (mmu_mem_type_q)
                     `_MEM_FETCH:
                         ecode = `_ECODE_PIF;
                     `_MEM_LOAD:
@@ -136,7 +139,7 @@ always_comb begin
             end else if(csr.crmd[`_CRMD_PLV] > tlb_value_read.plv) begin
                 trans_result.valid = 0;
                 ecode = `_ECODE_PPI;
-            end else if((mmu_mem_type == `_MEM_STORE) && (!tlb_value_read.d)) begin//fixed
+            end else if((mmu_mem_type_q == `_MEM_STORE) && (!tlb_value_read.d)) begin//fixed
                 trans_result.valid = 0;
                 ecode = `_ECODE_PME;
             end
@@ -145,14 +148,23 @@ always_comb begin
 end
 
 always_ff @(posedge clk) begin
-    if (~rst_n | flush) begin
-        trans_result_o <= '0;
-        tlb_exception_o <= '0;
-    end else begin
-        trans_result_o <= trans_result;
-        tlb_exception_o.ecode <= ecode;
-        tlb_exception_o.esubcode <= esubcode;
-    end
-end
+    va_q <= va;
+    mmu_mem_type_q <= mmu_mem_type;
+end//没有reset，外面需要有效位保证
+
+assign trans_result_o = trans_result;
+assign tlb_exception_o.ecode = ecode;
+assign tlb_exception_o.esubcode = esubcode;
+
+// always_ff @(posedge clk) begin
+//     if (~rst_n | flush) begin
+//         trans_result_o <= '0;
+//         tlb_exception_o <= '0;
+//     end else begin
+//         trans_result_o <= trans_result;
+//         tlb_exception_o.ecode <= ecode;
+//         tlb_exception_o.esubcode <= esubcode;
+//     end
+// end
 
 endmodule
